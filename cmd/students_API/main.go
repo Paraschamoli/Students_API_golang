08838bd58eadbc7eaca6ad53659ff433d7496734
paras chamoli <paraschamoli2592002@gmail.com>
2025-06-05 19:13:24 +0530

    adding comments

--- cmd/students_API/main.go
+++ cmd/students_API/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Paraschamoli/students_API/internal/config"
+)
+
+func main() {
+	//load config
+	cfg := config.MustLoad()
+	//database setup
+	//setup router
+	router:=http.NewServeMux()
+
+	router.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+	//setup server
+	server:=http.Server{
+		Addr: cfg.HTTPServer.Address,
+		Handler:router,
+	}
+	fmt.Printf("server started %s",cfg.HTTPServer.Address)
+	err:=server.ListenAndServe()
+	if err !=nil{
+		log.Fatal("failed to start server")
+	}
+
 	
 }
